8c860afc24d38253832d1e1e8dfed3bef47fc0dd
Denys Dzhulai <denisjuly@gmail.com>
2024-09-15 21:32:39 -0600

    Styled Skills and Projects setions with flexbox layout

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ const copyright = document.createElement("p");
 copyright.innerHTML = `© Denys Dzhulai ${thisYear} `;
 footer.appendChild(copyright);
 
-// Modifying "Skills" section using DOM manipulations
+// Modifying "Skills" section using DOM
 
 const skills = ["JavaScript", "HTML", "CSS", "GitHub"];
 const skillsSection = document.getElementById("skills");
@@ -59,7 +59,6 @@ messageForm.addEventListener("submit", function (event) {
 });
 
 // *Lesson #15*
-
 const GITHUB_USERNAME = "denysdzhulai";
 const API_URL = `https://api.github.com/users/${GITHUB_USERNAME}/repos`;
 
@@ -80,7 +79,18 @@ function displayRepositories(repositories) {
 
   repositories.forEach((repo) => {
     const project = document.createElement("li");
-    project.innerText = repo.name;
+
+    // Create a link element
+    const link = document.createElement("a");
+    link.href = repo.html_url;
+    link.innerText = repo.name;
+    link.target = "_blank";
+    link.rel = "noopener noreferrer";
+
+    // Append the link to the list item
+    project.appendChild(link);
+
+    // Append the list item to the project list
     projectList.appendChild(project);
   });
 }
